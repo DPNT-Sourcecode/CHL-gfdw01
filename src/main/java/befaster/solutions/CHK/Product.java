96b8cd92de8a378847c71b1dee0ab9fc8f309dc0
sbx_user1059 <sbx_user1059@ip-10-129-51-76.eu-west-2.compute.internal>
2019-10-12 15:02:42 +0000

    Sat Oct 12 15:02:42 UTC 2019

--- src/main/java/befaster/solutions/CHK/Product.java
+++ src/main/java/befaster/solutions/CHK/Product.java
@@ -1,19 +1,32 @@
 package befaster.solutions.CHK;
 
 public enum Product {
-	A(50),
-	B(30),
+	A(50,Promotion.PROMOTION_A),
+	B(30,Promotion.PROMOTION_B),
 	C(20),
 	D(15);
 
 	private Integer price;
 
+	private Promotion promotion;
+
 	Product(Integer price){
 		this.price = price;
+	}
+
+	Product(Integer price,Promotion promotion) {
+		this(price);
+		this.promotion = promotion;
 	}
 
 	public Integer getPrice() {
 
 		return this.price;
 	}
+
+	public Promotion getPromotion() {
+
+		return this.promotion;
+	}
 }
+
--- src/main/java/befaster/solutions/CHK/Promotion.java
+++ src/main/java/befaster/solutions/CHK/Promotion.java
@@ -1,4 +1,26 @@
 package befaster.solutions.CHK;
 
 public enum Promotion {
+
+	PROMOTION_A(3,130),
+
+	PROMOTION_B(2,45);
+
+	private Integer quantity;
+
+	private Integer newPrice;
+
+	Promotion(Integer quantity,Integer newPrice) {
+		this.quantity = quantity;
+		this.newPrice = newPrice;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public Integer getNewPrice() {
+		return newPrice;
+	}
 }
+
