b642b2cfa3a627d6842a8373b1931f863c601b15
sbx_user1059 <sbx_user1059@ip-10-129-51-76.eu-west-2.compute.internal>
2019-10-12 14:47:42 +0000

    Sat Oct 12 14:47:42 UTC 2019

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -1,5 +1,8 @@
 package befaster.solutions.CHK;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CheckoutSolution {
 
     private InputValidationService validationService;
@@ -18,12 +21,32 @@ public class CheckoutSolution {
             return -1;
         }
 
-        checkOut = compute(skus);
+        Map<Product,Integer> mappedInput = computeInputMap(skus);
 
 
         return checkOut;
     }
 
+    private Map<Product,Integer> computeInputMap(String skus) {
+
+        Map<Product,Integer> mappedInput = new HashMap<>();
+
+        for(int i=0;i<skus.length();i++){
+
+            Product key = Product.valueOf(skus.charAt(i) + "");
+
+            mappedInput.putIfAbsent(key,0);
+
+            Integer currentNumberOfProducts = mappedInput.get(key);
+
+            mappedInput.put(key,++currentNumberOfProducts);
+
+        }
+
+        return mappedInput;
+    }
+
 
 }
+
 
