3b2bc4213137f851f80edff9ec527de8eabc903a
sbx_user1059 <sbx_user1059@ip-10-129-51-76.eu-west-2.compute.internal>
2019-10-12 14:56:42 +0000

    Sat Oct 12 14:56:42 UTC 2019

--- src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
@@ -36,10 +36,50 @@ public class CheckoutSolutionTest {
 	}
 
 	@Test
-	public void givenSimpleValidInputWhenCheckoutThenShowResult(){
+	public void givenProductAWhenCheckoutThenShowResult(){
 
 		Integer checkout = checkoutSolution.checkout("A");
 
 		MatcherAssert.assertThat(checkout, CoreMatchers.equalTo(50));
 	}
+
+	@Test
+	public void givenProductBWhenCheckOutThenShowResult() {
+
+		Integer checkout = checkoutSolution.checkout("B");
+		MatcherAssert.assertThat(checkout, CoreMatchers.equalTo(30));
+	}
+
+	@Test
+	public void givenProductCWhenCheckOutThenShowResult() {
+
+		Integer checkout = checkoutSolution.checkout("C");
+		MatcherAssert.assertThat(checkout, CoreMatchers.equalTo(20));
+	}
+
+	@Test
+	public void givenProductDWhenCheckOutThenShowResult() {
+
+		Integer checkout = checkoutSolution.checkout("D");
+		MatcherAssert.assertThat(checkout, CoreMatchers.equalTo(15));
+	}
+
+	@Test
+	public void givenCombinedProductsWhenCheckOutThenShowResult() {
+		Integer checkout = checkoutSolution.checkout("ABCD");
+		MatcherAssert.assertThat(checkout, CoreMatchers.equalTo(115));
+	}
+
+	@Test
+	public void givenTwoAProductsWhenCheckoutThenShowResult() {
+		Integer checkout = checkoutSolution.checkout("AA");
+		MatcherAssert.assertThat(checkout, CoreMatchers.equalTo(100));
+	}
+
+	@Test
+	public void givenThreeAProductsWhenCheckoutThenShowResult() {
+		Integer checkout = checkoutSolution.checkout("AAA");
+		MatcherAssert.assertThat(checkout, CoreMatchers.equalTo(130));
+	}
 }
+
