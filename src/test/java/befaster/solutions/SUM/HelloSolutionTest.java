04c4b8416a0e65710707c477f4ae6ded09dec7ad
sbx_user1059 <sbx_user1059@ip-10-129-51-76.eu-west-2.compute.internal>
2019-10-12 13:44:14 +0000

    Sat Oct 12 13:44:14 UTC 2019

--- src/test/java/befaster/solutions/SUM/HelloSolutionTest.java
+++ src/test/java/befaster/solutions/SUM/HelloSolutionTest.java
@@ -1,7 +1,33 @@
 package befaster.solutions.SUM;
 
+import befaster.solutions.HLO.HelloSolution;
+import org.hamcrest.MatcherAssert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+
 /**
+ * Test class for {@link befaster.solutions.HLO.HelloSolution}
+ *
  * @author acozma
  */
 public class HelloSolutionTest {
+
+	private HelloSolution helloSolution;
+
+	@Before
+	public void setUp() {
+
+		helloSolution = new HelloSolution();
+	}
+
+	@Test
+	public void givenHelloSolutionInstanceWhenRunningHelloThenReturnCorrectMessage() {
+
+		MatcherAssert.assertThat(helloSolution.hello("Alex"),equalTo("hello world!"));
+	}
 }
+
